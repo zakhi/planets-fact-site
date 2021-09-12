65ad0ea2ff21e8d291251d49f74f77a09d317212
Zakhi Verech <zakhi@omnisol.co.il>
2021-09-12 11:24:28 +0300

    Add javascript to replace content and image based on user selections

---
+++ javascript/controls.js
@@ -0,0 +1,28 @@
+import { updateData } from "./update-data.js";
+
+document.querySelectorAll("nav a").forEach(planetLink => {
+    const planet = planetLink.parentElement.id
+
+    planetLink.addEventListener("click", e => {
+        e.preventDefault()
+
+        document.body.dataset.planet = planet
+        updateData()
+    })
+})
+
+const tabLinks = document.querySelectorAll(".tabs a");
+
+tabLinks.forEach(tabLink => {
+    const tab = tabLink.parentElement.id
+
+    tabLink.addEventListener("click", e => {
+        e.preventDefault()
+
+        document.querySelector(".tabs .selected").classList.remove("selected")
+        tabLink.parentElement.classList.add("selected")
+
+        document.body.dataset.tab = tab
+        updateData()
+    })
+})
---
+++ javascript/update-data.js
@@ -0,0 +1,20 @@
+const data = await fetch("../data.json").then(response => response.json())
+
+export function updateData() {
+    const planet = document.body.dataset.planet
+    const tab = document.body.dataset.tab
+    const planetData = data.find(e => e.name.toLowerCase() === planet)
+
+    document.documentElement.style.setProperty("--color-primary", `var(--color-${planet})`)
+
+    const imageType = tab === "structure" ? "internal" : "planet"
+    document.getElementById("planet-image").setAttribute("src", planetData.images[imageType])
+
+    document.getElementById("planet-name").textContent = planetData.name
+    document.getElementById("planet-description").textContent = planetData[tab].content
+    document.getElementById("planet-link").setAttribute("href", planetData[tab].source)
+    document.getElementById("planet-rotation").textContent = planetData.rotation
+    document.getElementById("planet-revolution").textContent = planetData.revolution
+    document.getElementById("planet-radius").textContent = planetData.radius
+    document.getElementById("planet-temperature").textContent = planetData.temperature
+}
